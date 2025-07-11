6204b3e96915cc33c08ab6370b2389cabe0f182f
Hristiyan <hristiyan.valkov@limechain.tech>
2025-07-11 15:06:42 +0300

    feat: expo setup
    Signed-off-by: Hristiyan <hristiyan.valkov@limechain.tech>

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Layout;
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;

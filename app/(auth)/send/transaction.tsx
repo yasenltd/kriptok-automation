e04a289ff9bb68ca8093e1c1dad7711d28ed0907
Hristiyan <hristiyan.valkov@limechain.tech>
2025-08-07 10:22:47 +0300

    feat: send flow
    Signed-off-by: Hristiyan <hristiyan.valkov@limechain.tech>

---
+++ app/(auth)/send/transaction.tsx
@@ -0,0 +1,12 @@
+import TransactionScreen from '@/screens/send/TransactionScreen';
+import { View } from 'react-native';
+
+const Transaction = () => {
+  return (
+    <View style={{ flex: 1, padding: 20 }}>
+      <TransactionScreen />
+    </View>
+  );
+};
+
+export default Transaction;
